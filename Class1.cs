8f8709d9c226a5d810b579b13f099acf1a240185
nathan <ennemmjones@gmail.com>
2021-10-06 19:53:33 -0400

    Package practice

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+namespace firstLibrary
+{
+    
+    public class Hello
+    {
+        public string Value { get; set; }
+        public Hello() { }
+    }
+    public class Class1
+    {
+        public string Serialize()
+        {
+            Hello testObject = new Hello() { Value = "this is a test" };
+            return JsonConvert.SerializeObject(testObject);
+        }
+    }
+}
